fa1dd9398e48682ae6d25525564149000a0b86b2
datnq <datnq@gemvietnam.com>
2025-01-08 23:16:38 +0700

    [datnq] fix layout Features Section

--- src/GlobalStar.tsx
+++ src/GlobalStar.tsx
@@ -182,8 +182,8 @@ export const GlobalStar = () => {
 
         {/* Features Section */}
         <section className="overflow-hidden px-20 py-16 w-full bg-white max-md:px-5 max-md:max-w-full">
-          <div className="flex gap-5 max-md:flex-col">
-            <div className="flex flex-col w-6/12 max-md:ml-0 max-md:w-full">
+          <div className="flex gap-5 max-md:flex-col justify-center">
+            <div className="flex flex-col w-4/12 max-md:ml-0 max-md:w-full">
               <div className="flex flex-col max-md:mt-10 max-md:max-w-full">
                 <div className="flex flex-col max-w-full w-[571px]">
                   <h2 className="text-4xl font-semibold tracking-tighter leading-[52px] text-stone-950 max-md:max-w-full">
@@ -207,7 +207,7 @@ export const GlobalStar = () => {
                 </div>
               </div>
             </div>
-            <div className="flex flex-col ml-5 w-6/12 max-md:ml-0 max-md:w-full">
+            <div className="flex flex-col ml-5 w-4/12 max-md:ml-0 max-md:w-full">
               <div className="flex flex-col mt-6 w-full max-md:mt-10 max-md:max-w-full">
                 {features.map((feature, index) => (
                   <FeatureCard key={index} {...feature} />
